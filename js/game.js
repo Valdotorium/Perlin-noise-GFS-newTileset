62a324e1185f7ebd09733de16993a752cff87812
Jorandint <116171114+Jorandint@users.noreply.github.com>
2025-12-29 17:50:14 +0100

    e2

--- js/game.js
+++ js/game.js
@@ -42,13 +42,6 @@ class NoiseScene extends Phaser.Scene
         
         const camera = this.cameras.main;
         camera.setZoom(1);
-        //this.cameras.main.setBounds();
-        //const tileset = map.addTilesetImage('tile');
-        //const layer = map.createBlankLayer(0, tileset, 0, 0);
-        //layer.setScale(4);
-        //layer.randomize(0, 0, map.width, map.height, [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 19, 20, 23, 24, 25 ]);
-        //camera.setZoom(1);
-        //console.log(map)
         this.cameras.main.setBounds();
     }
     update ()
